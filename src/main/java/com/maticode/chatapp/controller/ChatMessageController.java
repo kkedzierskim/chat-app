a173c69a0d3a8cf3a907cf8c046732f1e7941fd3
Mateusz <kkedzierski.m@gmail.com>
2019-12-22 19:33:25 +0100

    simple implementation

---
+++ src/main/java/com/maticode/chatapp/controller/ChatMessageController.java
@@ -0,0 +1,18 @@
+package com.maticode.chatapp.controller;
+
+
+import com.maticode.chatapp.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatMessageController {
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public ChatMessage get(ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+}
