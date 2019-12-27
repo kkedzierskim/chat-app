c676bc887dab3c8d41febe93949ccf36dea410eb
Mateusz <kkedzierski.m@gmail.com>
2019-12-27 22:23:55 +0100

    MessageType enum added

--- src/main/java/com/maticode/chatapp/controller/ChatMessageController.java
+++ src/main/java/com/maticode/chatapp/controller/ChatMessageController.java
@@ -1,7 +1,7 @@
 package com.maticode.chatapp.controller;
 
 
-import com.maticode.chatapp.ChatMessage;
+import com.maticode.chatapp.domain.ChatMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
---
+++ src/main/java/com/maticode/chatapp/domain/MessageType.java
@@ -0,0 +1,7 @@
+package com.maticode.chatapp.domain;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
